685db2ac8c55c2c17704e85d222ee2412fe20d12
brandon-m-navarro <brandon.m.navarro@gmail.com>
2024-10-13 22:00:50 -0400

    installed new dependencies

--- brandon-nextjs/app/ui/theme-toggle.tsx
+++ brandon-nextjs/app/ui/theme-toggle.tsx
@@ -7,9 +7,17 @@ export default function ThemeToggle({
 }: {
   isDayMode: boolean;
 }) {
+
+    let clickHandler = function (event) {
+        // Check ehich button was selected
+        let mode = event.detail.dataset.mode;
+        
+    }
+
   return (
     <div className="flex w-[150px] justify-around">
       <div
+        data-mode='day'
         className={clsx(
           "rounded-full opacity-50 hover:opacity-75 transition-all bg-green w-[36px] h-[36px] flex justify-center items-center cursor-pointer",
           {
@@ -17,9 +25,10 @@ export default function ThemeToggle({
           }
         )}
       >
-        <img src={Sun} className="w-[24px] h-[24px]"></img>
+        <img src={Sun} className="w-[24px] h-[24px] pointer-events-none"></img>
       </div>
       <div
+        data-mode='night'
         className={clsx(
           "rounded-full opacity-25 hover:bg-arsenic hover:opacity-75 transition-all bg-arsenic w-[36px] h-[36px] flex justify-center items-center cursor-pointer",
           {
@@ -27,7 +36,7 @@ export default function ThemeToggle({
           }
         )}
       >
-        <img src={Moon} className="w-[24px] h-[24px]"></img>
+        <img src={Moon} className="w-[24px] h-[24px] pointer-events-none"></img>
       </div>
     </div>
   );
