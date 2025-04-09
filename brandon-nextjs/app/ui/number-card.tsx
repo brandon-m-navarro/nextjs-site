c1c6a7c674a98af750a2fd2ac8c0a45fba5e97d4
Brandon <brandon.m.navarro@gmail.com>
2025-04-09 16:47:54 -0400

    fallout fix

--- brandon-nextjs/app/ui/number-card.tsx
+++ brandon-nextjs/app/ui/number-card.tsx
@@ -3,8 +3,8 @@
 export default function NumberedCard ({number, title, content, className} : {number: number, title: string, content: string, className?:string}) {
 
     return (
-        <div className={"flex flex-col !text-black dark:text-white transition-colors" + ' ' + className}>
-            <span className="!text-dayOrange text-[28px] h-[32px] dark:text-nightGold">
+        <div className={"flex flex-col !text-black dark:!text-white transition-colors" + ' ' + className}>
+            <span className="!text-dayOrange text-[28px] h-[32px] dark:!text-nightGold">
                 {number}
             </span>
             <span className="font-semibold text-[24px] whitespace-nowrap h-[32px] mb-[12px] mt-[12px]">{title}</span>
