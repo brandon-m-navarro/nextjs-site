c32a04e9ca5bf623e57393841470b86695a665e8
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-10-22 23:29:49 -0400

    Added metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,16 @@
 import "./globals.css";
 import Header from "./ui/header";
 import { josefin } from "@/app/ui/fonts";
+import type { Metadata } from "next";
 
 import { ThemeContextProvider } from "./store/ThemeContext";
 import Footer from "./ui/footer";
+
+
+export const metadata: Metadata = {
+  title: "Brandon Navarro",
+  description: "My portfolio site built with Next.js and TypeScript.",
+};
 
 export default function RootLayout({
   children,
